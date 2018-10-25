d9d4317592e561467247bd83e34f7b6b0546db33
ChrisL_frakkass <43646777+dclchrislfrakkass@users.noreply.github.com>
2018-10-25 19:07:00 +0200

    first commit with some changes

---
+++ script.js
@@ -0,0 +1,90 @@
+// Variable //
+var lancerJeux;
+var quiJoue;
+var choixCaseJoueur1;
+var choixCaseJoueur2;
+var finJeux;
+var relancer;
+
+var aa = document.getElementById("1:1").addEventListener("click", joue);
+var ab =  document.getElementById("1:2").addEventListener("click", joue);
+var ac = document.getElementById("1:3").addEventListener("click", joue);
+var ad = document.getElementById("1:4").addEventListener("click", joue);
+var ae = document.getElementById("1:5").addEventListener("click", joue);
+var af = document.getElementById("1:6").addEventListener("click", joue);
+var ag = document.getElementById("1:7").addEventListener("click", joue);
+
+var ba = document.getElementById("2:1").addEventListener("click", joue);
+var bb =  document.getElementById("2:2").addEventListener("click", joue);
+var bc = document.getElementById("2:3").addEventListener("click", joue);
+var bd = document.getElementById("2:4").addEventListener("click", joue);
+var be = document.getElementById("2:5").addEventListener("click", joue);
+var bf = document.getElementById("2:6").addEventListener("click", joue);
+var bg = document.getElementById("2:7").addEventListener("click", joue);
+
+var ca = document.getElementById("3:1").addEventListener("click", joue);
+var cb =  document.getElementById("3:2").addEventListener("click", joue);
+var cc = document.getElementById("3:3").addEventListener("click", joue);
+var cd = document.getElementById("3:4").addEventListener("click", joue);
+var ce = document.getElementById("3:5").addEventListener("click", joue);
+var cf = document.getElementById("3:6").addEventListener("click", joue);
+var cg = document.getElementById("3:7").addEventListener("click", joue);
+
+var da = document.getElementById("4:1").addEventListener("click", joue);
+var db =  document.getElementById("4:2").addEventListener("click", joue);
+var dc = document.getElementById("4:3").addEventListener("click", joue);
+var dd = document.getElementById("4:4").addEventListener("click", joue);
+var de = document.getElementById("4:5").addEventListener("click", joue);
+var df = document.getElementById("4:6").addEventListener("click", joue);
+var dg = document.getElementById("4:7").addEventListener("click", joue);
+
+var ea = document.getElementById("5:1").addEventListener("click", joue);
+var eb =  document.getElementById("5:2").addEventListener("click", joue);
+var ec = document.getElementById("5:3").addEventListener("click", joue);
+var ed = document.getElementById("5:4").addEventListener("click", joue);
+var ee = document.getElementById("5:5").addEventListener("click", joue);
+var ef = document.getElementById("5:6").addEventListener("click", joue);
+var eg = document.getElementById("5:7").addEventListener("click", joue);
+
+var fa = document.getElementById("6:1").addEventListener("click", joue);
+var fb =  document.getElementById("6:2").addEventListener("click", joue);
+var fc = document.getElementById("6:3").addEventListener("click", joue);
+var fd = document.getElementById("6:4").addEventListener("click", joue);
+var fe = document.getElementById("6:5").addEventListener("click", joue);
+var ff = document.getElementById("6:6").addEventListener("click", joue);
+var fg = document.getElementById("6:7").addEventListener("click", joue);
+
+var ga = document.getElementById("7:1").addEventListener("click", joue);
+var hb =  document.getElementById("7:2").addEventListener("click", joue);
+var gc = document.getElementById("7:3").addEventListener("click", joue);
+var gd = document.getElementById("7:4").addEventListener("click", joue);
+var ge = document.getElementById("7:5").addEventListener("click", joue);
+var gf = document.getElementById("7:6").addEventListener("click", joue);
+var gg = document.getElementById("7:7").addEventListener("click", joue);
+
+var x;
+var y;
+// Démmarage du jeu //
+// Joueur 1 Joue //
+
+
+
+
+
+function joue(event) {
+    console.log(event.target.id);
+    
+
+
+}
+// Récupération coordonés pions joueur 1 //
+// Verifier si vide //
+// Apparition du pion //
+// Joueur 2 Joue //
+//Récupération coordonés pions joueur 2 //
+// Verifier si vide //
+// Apparition du pion //
+// Analyse du nombre de pion aligné //
+// Si 4 pions alignés //
+// Si plus de pions //
+// Restrat//
