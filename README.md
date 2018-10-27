c462a95a32a750895f6473c581381df36cf02be1
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2018-10-27 17:50:57 +0200

    js nice advance

--- README.md
+++
@@ -1,2 +0,0 @@
-# puissance-4
-project 2 with javascript css and html
--- script.js
+++ script.js
@@ -1,95 +1,119 @@
-// Variable //
-var lancerJeux;
-var quiJoue;
-var joueurEnCour;
-var choixCaseJoueur1;
-var choixCaseJoueur2;
-var finJeux;
-var relancer;
-var cola = document.getElementById("col1");
-var myTable = document.getElementById("myTable");
-var x;
-var y;
-var cell =0;
-var choice = document.getElementById("td");
+const debug = true; // mode debug
+var player=1; //1 for Yellow (player1), 2 for Red (player2)
+var jeu = [
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+];
 
 
-// cola.addEventListener("click",     
-// function() {
-//     jeu[x][y]="1";
-//     truc.style.backgroundColor="red";
-//     console.log(jeu);
-// });
-// myTable.addEventListener("click",
-// function () {
-//   x = this.id;
-//   myTable.style.backgroundColor="red";
-// });
+//verify the player
+function selectColumn(col) {
+	
+	if (player==1) {
+		jeu[6][col]=1;
+		player=2;
+		document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
+	} else {
+		jeu[6][col]=2;
+		player=1;
+		document.getElementById("colorTurn").innerHTML="Tour : Joueur 1";
+	}
 
-var jeu= [[0,0,0,0,0,0,0,],
-         [0,0,0,0,0,0,0,],
-         [0,0,0,0,0,0,0,],
-         [0,0,0,0,0,0,0,],
-         [0,0,0,0,0,0,0,],
-         [0,0,0,0,0,1,0,],
-         [0,0,0,0,0,0,0,]];
-
-
-// Démmarage du jeu //
-// Joueur 1 Joue //
-joueurEnCour = 1;
-
-var table = document.getElementById("myTable"),rIndex,cIndex;
-            
-// table rows
-for(var i = 0; i < table.rows.length; i++)
-{
-    // row cells
-    for(var j = 0; j < table.rows[i].cells.length; j++)
-    {
-        table.rows[i].cells[j].onclick = function()
-        {
-            rIndex = this.parentElement.rowIndex;
-            cIndex = this.cellIndex;
-            console.log("Row : "+rIndex+" , Cell : "+cIndex);
-            console.log(jeu[rIndex][cIndex]);
-            jeu[rIndex][cIndex]= 1;
-            console.log(jeu[rIndex][cIndex]);
-            jeu.forEach(Number => console.log(Number));
-           
-        };
-    }
+	placeColor();
 }
 
-table.addEventListener("click",
-        function (e) {
-            console.log(e);
-            console.log(e.target);
 
-        }
-        
+//to place a color on board
+function placeColor() {
+	for (var row = 0; row < 7; row++) {
+		for (var col = 0; col < 7; col++) {
+			if (jeu[row][col]==0) { 
+				document.getElementById("cell"+row+col).style.backgroundColor="white";
+			} else if (jeu[row][col]==1) { 
+				document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+			} else if (jeu[row][col]==2) { 
+				document.getElementById("cell"+row+col).style.backgroundColor="red";
+			}
+			// checkCol();
+		}
+	}  
+}
 
-)
-// jeu.forEach(function(callback) {
-//         document.getElementById("myTable").cells;
-//         backgroundColor="red";
-// });
+//verify if an cell is taken
+// function checkCol(col) {
 
 
-// function joueur1(rIndex,cIndex) {
-//     td.style.backgroundColor="red";
 
-// }  
-    
+var reset = document.getElementById("resetButton");
+resetButton.addEventListener("click",
+    function resetGame(){
+        location.reload();
+});
+
+// //reset the game to restart
+// function resetGrid() {
+// 	var jeu = [
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 		[0, 0, 0, 0, 0, 0, 0],
+// 	];
+// 	placeColor();
 // }
-// Récupération coordonés pions joueur 1 //
-// Verifier si vide //
-// Apparition du pion //
-// Joueur 2 Joue //
-//Récupération coordonés pions joueur 2 //
-// Verifier si vide //
-// Apparition du pion //
-// Analyse du nombre de pion aligné //
-// Si 4 pions alignés //
-// Si plus de pions //
-// Restrat//
+var reset = document.getElementById("resetButton");
+resetButton.addEventListener("click",
+    function resetGame(){
+        location.reload();
+});
+
+
+
+
+// var joueur=1; //1 pour joueur 1 et 2 pour joueur 2
+// var jeu= [[0,0,0,0,0,0,0,],
+//          [0,0,0,0,0,0,0,],
+//          [0,0,0,0,0,0,0,],
+//          [0,0,0,0,0,0,0,],
+//          [0,0,0,0,0,0,0,],
+//          [0,0,0,0,0,1,0,],
+//          [0,0,0,0,0,0,0,]];
+
+
+
+// var table = document.getElementById("myTable"),rIndex,cIndex;
+
+// // table rows
+// for(var i = 0; i < table.rows.length; i++)
+// {
+//     // row cells
+//     for(var j = 0; j < table.rows[i].cells.length; j++)
+//     {
+//         table.rows[i].cells[j].onclick = function()
+//         {
+//             rIndex = this.parentElement.rowIndex;
+//             cIndex = this.cellIndex;
+//             console.log("Row : "+rIndex+" , Cell : "+cIndex);
+//             console.log(jeu[rIndex][cIndex]);
+//             jeu[rIndex][cIndex]= 1;
+//             console.log(jeu[rIndex][cIndex]);
+//             jeu.forEach(Number => console.log(Number));
+
+//         };
+//     }
+// }
+// var event;
+// table.addEventListener("click",
+//         function (e) {
+//             console.log(e.target);
+//             event = e.target;
+//         }
+// );
+
