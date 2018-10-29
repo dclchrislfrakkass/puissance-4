ce3972cffd2e35ffd24295c4f9bb1600184141c5
ChrisL_frakkass <43646777+dclchrislfrakkass@users.noreply.github.com>
2018-10-29 20:36:36 +0100

    ok with rows cols and player turn

--- script.js
+++ script.js
@@ -11,39 +11,16 @@ var jeu = [
 	[0, 0, 0, 0, 0, 0, 0],
 ];
 
-// function firstFreeRow(col,player){
-// 	for(var i = 0; i<6; i++){
-// 	  if(jeu[i][col]!=0){
-// 		break;
-// 	  }
-// 	}
-// 	jeu[i-1][col] = player;
-// 	return i-1;
-
-
 
 
 ///work/////
-//verify the player
-// function selectColumn(col) {
 
-//	function dropToBottom(row, col) {
-// Start at the bottom of the column, and step up, checking to make sure
-// each position has been filled. If one hasn't, return the empty position.
-// for (var y = 6; y > row; y--) {
-// 	if (jeu[y][col] === 0) {
-// 		return y;
-// 	}
-// }
 
-/* drawBoard will draw the board - it will update each item to make sure it is the appropriate value */
 
 function drawBoard() {
 	// checkForWin(); //check to see if any player has won.
 	for (col = 0; col<=6; col++) {
-		for (row=0; row<=6; row++) {
-			//Set the inner HTML of the square (a td) to be a span with the class of 'piece' and 'player' + the value of that 
-			//gameboard piece.  Using CSS, you can style player0, player1 and player2 so that the square will appear correctly.
+		for (row = 0; row<=6; row++) {
 			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
 				document.getElementById("cell"+row+col).style.backgroundColor="white";
 			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
@@ -54,10 +31,14 @@ function drawBoard() {
 		}	
 	}
 }
-/* drop will add a piece to the lowest available column */
+
+function displayPlay() {
+	document.getElementById("colorTurn").innerHTML="Tour : Joueur "+player;
+	}
+
+// add a piece to the lowest available column 
 function selectColumn(col) {
-	/* Look for the lowest point in this row that is open */
-	//the opposite of our loop above - as we're going to start from the bottom looking for an open slot
+	// Look for the lowest point in this row that is open
 	for (row=6; row>=0; row--) { //note: we are using row--, which will reduce the value of row by 1, the opposted of ++
 		if (jeu[row][col] == 0) {
 			//set the empty row to the active player's number
@@ -69,9 +50,8 @@ function selectColumn(col) {
 			} else {
 				player = 1;
 			}
-			//there is also a fancy way of doing this call a ternary assignment that looks like this: active_player = (active_player == 1) ? 2 : 1;
+			displayPlay();
 			
-			setUpTurn(); //display who is the active player
 			//stop looking for empty spaces
 			return true;
 		}
@@ -112,24 +92,24 @@ function selectColumn(col) {
 
 
 //to place a color on board
-function placeColor() {
-	for (var row = 0; row < 7; row++) {					//verify if row is between 0 and 7
-		for (var col = 0; col < 7; col++) {				//verify if column is between 0 and 7
-			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
-				document.getElementById("cell"+row+col).style.backgroundColor="white";
-			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
-				document.getElementById("cell"+row+col).style.backgroundColor="yellow";
-			} else if (jeu[row][col]==2) { 													//verify if the number in the array is 0
-				document.getElementById("cell"+row+col).style.backgroundColor="red";
-			}
-			// else if (jeu[row][col]!=0) {
-			// 	for (row =0; row < 7; row--) {
-			// 		document.getElementById("cell"+row+col).style.backgroundColor="yellow";
-			// 	}
-			// }
-		}
-	}  
-}
+// function placeColor() {
+// 	for (var row = 0; row < 7; row++) {					//verify if row is between 0 and 7
+// 		for (var col = 0; col < 7; col++) {				//verify if column is between 0 and 7
+// 			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
+// 				document.getElementById("cell"+row+col).style.backgroundColor="white";
+// 			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
+// 				document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+// 			} else if (jeu[row][col]==2) { 													//verify if the number in the array is 0
+// 				document.getElementById("cell"+row+col).style.backgroundColor="red";
+// 			}
+// else if (jeu[row][col]!=0) {
+// 	for (row =0; row < 7; row--) {
+// 		document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+// 	}
+// 			// }
+// 		}
+// 	}  
+// }
 
 
 
